56ff8e2c7ad8a2055f9e62c62aaff1370c4143ba
Tiffani Rice <tiffanirice414@gmail.com>
2023-04-10 18:22:30 -0700

    Final Edits

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 console.log('hi, Sheyna and Kale!');
 
-let hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 a.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', 'Total']
+let hours = ['6 a.m', '7 a.m', '8 a.m', '9 a.m', '10 a.m', '11 a.m', '12 p.m', '1 p.m', '2 p.m', '3 p.m', '4 p.m', '5 p.m', '6 p.m', '7 p.m', 'Total']
 // Seattle Store
 let seattleContainer = document.getElementById("seattleList");
 let seattleStoreObj = {
